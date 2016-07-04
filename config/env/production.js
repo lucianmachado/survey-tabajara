02d727351e6dcbed1f9573d929196c1489cbed37
Lucian Machado <lucian.machado@fundacred.org.br>
2016-07-04 00:05:50 -0300

    Production config

--- config/env/production.js
+++ config/env/production.js
@@ -8,7 +8,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 80,
 
   log: {
     level: "silent"
